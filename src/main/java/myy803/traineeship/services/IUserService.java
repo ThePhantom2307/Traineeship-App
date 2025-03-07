7d7281fadefa48cb9c8a227018a17f912872d611
Panagiotis T. <49351300+ThePhantom2307@users.noreply.github.com>
2025-03-07 23:00:38 +0200

    Delete src directory

--- src/main/java/myy803/traineeship/services/IUserService.java
+++
@@ -1,9 +0,0 @@
-package myy803.traineeship.services;
-
-import myy803.traineeship.model.User;
-
-public interface IUserService {
-	void saveUser(User user);
-	User findById(String username);
-	boolean isUserPresent(User user);
-}
