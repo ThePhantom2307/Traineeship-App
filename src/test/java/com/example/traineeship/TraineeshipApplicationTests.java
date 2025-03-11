574d0c2321655b6457b1f37d33d8d9462ca8a806
Panagiotis T. <49351300+ThePhantom2307@users.noreply.github.com>
2025-03-11 19:41:40 +0200

    Add files via upload

---
+++ src/test/java/com/example/traineeship/TraineeshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.traineeship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
